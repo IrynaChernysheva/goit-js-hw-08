3f3798f18f1beacb4ec48b275e3ad5f4948a97c8
IrynaChernysheva <100154794+IrynaChernysheva@users.noreply.github.com>
2022-09-01 13:34:26 +0300

    02-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+const onPlayer = function (evt) {
+    localStorage.setItem(`videoplayer-current-time`, evt.seconds);
+        
+        console.log('played the video!');
+};
+player.on(`timeupdate`, throttle(onPlayer, 1000));
+
+    player.setCurrentTime(localStorage.getItem(`videoplayer-current-time`)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
