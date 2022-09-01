e4a5fc09045e78984c2083e32241ccad418ae045
IrynaChernysheva <100154794+IrynaChernysheva@users.noreply.github.com>
2022-09-01 12:14:09 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryItemsEl = document.querySelector('.gallery');
+
+function galleryEl(galleryItems) {
+  const galleryElCard = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`,
+    )
+    .join('');
+
+  return galleryElCard;
+}
+
+galleryItemsEl.insertAdjacentHTML('beforeend', galleryEl(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
