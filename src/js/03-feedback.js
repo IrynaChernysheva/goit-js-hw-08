ac2df36aaeaaa0e47121ac4e8102edbb6686474e
IrynaChernysheva <100154794+IrynaChernysheva@users.noreply.github.com>
2022-09-02 15:00:23 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+const textInput = document.querySelector(".feedback-form");
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+textInput.addEventListener('input', throttle(onFormInput, 500));
+textInput.addEventListener('submit', onFormSubmit);
+formEl();
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    evt.currentTarget.reset();
+    console.log(formData);
+}
+function formEl() {
+    const inputEl = localStorage.getItem(STORAGE_KEY);
+    if (inputEl) {
+        inputEl = JSON.parse(inputEl);
+    } else {
+        return;
+    }
+    Object.entries(formEl).forEach(([name, value]) => (textInput.elements[name].value = value));
+}
+
+
+
 
