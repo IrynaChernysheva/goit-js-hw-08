0dcfe6481111098f9907f0369cc93b8911136844
IrynaChernysheva <100154794+IrynaChernysheva@users.noreply.github.com>
2022-09-04 15:52:44 +0300

    03-js-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,45 @@
 import throttle from 'lodash.throttle';
 
-const textInput = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+const textInput = document.querySelector('.feedback-form');
 
+
+textInput.addEventListener('submit', onSubmitForm);
 textInput.addEventListener('input', throttle(onFormInput, 500));
-textInput.addEventListener('submit', onFormSubmit);
 
-formEl();
 
-function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+function onFormInput() {
+  const formData = new FormData(textInput);
+  let formEl = {};
+  formData.forEach((value, name) => (formEl[name] = value.trim()));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formEl));
 }
-function onFormSubmit(evt) {
+
+
+dataFormEl();
+function dataFormEl() {
+  let parseForm = localStorage.getItem(STORAGE_KEY);
+  if (parseForm) {
+    parseForm = JSON.parse(parseForm);
+    Object.entries(parseForm).forEach(([name, value]) => {
+      textInput.elements[name].value = value;
+    });
+  }
+}
+
+
+function onSubmitForm(evt) {
   evt.preventDefault();
-  localStorage.removeItem(STORAGE_KEY);
-  evt.currentTarget.reset();
-  console.log(formData);
-}
-function formEl() {
-  let inputEl = localStorage.getItem(STORAGE_KEY);
-  if (inputEl) {
-    inputEl = JSON.parse(inputEl);
-  } else {
+  const inputName = textInput.email.value;
+  const inputMessage = textInput.message.value;
+  if (inputName && inputMessage !== '') {
+    let formEl = localStorage.getItem(STORAGE_KEY);
+    formEl = JSON.parse(formEl);
+    console.log(formEl);
+    localStorage.removeItem(STORAGE_KEY);
+    evt.currentTarget.reset();
     return;
   }
-  Object.entries(inputEl).forEach(
-    ([name, value]) => (textInput.elements[name].value = value)
-  );
+
+  return;
 }
